4bb61a5e089a1d8319c11e5bf9d61ba35d4a72b4
mgdasari <dasarimukund@gmail.com>
2021-04-09 21:04:15 +0530

    second commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 module.exports = (app) => {
   const book = require("../controller/controller");
 
-  app.post("/add", book.add);
-  app.post("/find", book.find);
-  app.post("/update", book.update);
-  app.post("/delete", book.delete);
+  app.post("/add", book.add); //Find the existence of detail if not save to database
+  app.post("/find", book.find); //Find all the data related to title
+  app.post("/update", book.update); //Update the data
+  app.post("/delete", book.delete); //delete the data.
 };
